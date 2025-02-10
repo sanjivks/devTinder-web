321b4fb6f264af6cfde12fca882c8ac68737893d
sanjivks <sanjivshinde4@gmail.com>
2025-02-11 00:40:07 +0530

    created Login page

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,11 +1,39 @@
-import React from 'react'
+import React from "react";
 
 const Login = () => {
   return (
-    <div>
-      <h1>Login</h1>
-    </div>
-  )
-}
+    <div className="flex justify-center my-10">
+      <div className="card bg-base-300 w-96 shadow-xl">
+        <div className="card-body">
+          <h2 className="card-title justify-center">Login</h2>
 
-export default Login
+          <div>
+            <label className="form-control w-full max-w-xs my-2">
+              <div className="label">
+                <span className="label-text">Email ID</span>
+              </div>
+              <input
+                type="text"
+                className="input input-bordered w-full max-w-xs"
+              />
+            </label>
+            <label className="form-control w-full max-w-xs my-2">
+              <div className="label">
+                <span className="label-text">Password</span>
+              </div>
+              <input
+                type="text"
+                className="input input-bordered w-full max-w-xs"
+              />
+            </label>
+          </div>
+          <div className="card-actions justify-center m-2">
+            <button className="btn btn-primary">Login</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
