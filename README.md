ef06f7c6263d0469f6d6da950fcaae0e2b54f7fc
sanjivks <sanjivshinde4@gmail.com>
2025-02-11 13:20:17 +0530

    axios installed and  firts login api started

--- README.md
+++ README.md
@@ -10,3 +10,7 @@
 - Create BrowserRouter > Routes > Route=/ Body > RouteChildren
 - Create an Outlet in your Body Component
 - Create a footer
+- Create a Login Page
+- Install axios
+- CORS - install cors in backend => add middleware to with configurations: orgin, credentials: true
+- Whenever you're making API call so pass axios => { withCredentials: true }
--- src/Login.jsx
+++ src/Login.jsx
@@ -1,6 +1,20 @@
-import React from "react";
+import axios from "axios";
+import React, { useState } from "react";
 
 const Login = () => {
+  const [emailId, setEmaiId] = useState("Ashu@gmail.com");
+  const [password, setPassword] = useState("Ashu@1231");
+  const handleLogin = async () => {
+    try {
+      const res = await axios.post("http://localhost:7777/login", {
+        emailId,
+        password,
+      },
+      {withCredentials:true});
+    } catch (err) {
+      console.log(err);
+    }
+  };
   return (
     <div className="flex justify-center my-10">
       <div className="card bg-base-300 w-96 shadow-xl">
@@ -10,11 +24,13 @@ const Login = () => {
           <div>
             <label className="form-control w-full max-w-xs my-2">
               <div className="label">
-                <span className="label-text">Email ID</span>
+                <span className="label-text">Email ID </span>
               </div>
               <input
                 type="text"
+                value={emailId}
                 className="input input-bordered w-full max-w-xs"
+                onChange={(e) => setEmaiId(e.target.value)}
               />
             </label>
             <label className="form-control w-full max-w-xs my-2">
@@ -23,12 +39,14 @@ const Login = () => {
               </div>
               <input
                 type="text"
+                value={password}
                 className="input input-bordered w-full max-w-xs"
+                onChange={(e) => setPassword(e.target.value)}
               />
             </label>
           </div>
           <div className="card-actions justify-center m-2">
-            <button className="btn btn-primary">Login</button>
+            <button className="btn btn-primary" onClick={handleLogin}>Login</button>
           </div>
         </div>
       </div>
