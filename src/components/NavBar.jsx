c238a82175ecdbccfdbe8ad81d2186c2d98a696f
sanjivks <sanjivshinde4@gmail.com>
2025-02-12 14:38:55 +0530

    logout functionlity completed

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,14 +1,28 @@
-import { useSelector } from "react-redux";
-import { Link } from "react-router-dom";
+import axios from "axios";
+import { useDispatch, useSelector } from "react-redux";
+import { Link,  useNavigate } from "react-router-dom";
+import { BASE_URL } from "../utils/constants";
+import { removeUser } from "../utils/userSlice";
 
 const NavBar = () => {
   const user = useSelector((store) => store.user);
-  console.log(user);
+  const dispacth=useDispatch();
+  const navigate=useNavigate()
+
+  const handleLogout = async () => {
+    try {
+      await axios.post(BASE_URL + "/logout", {}, { withCredentials: true });
+      dispacth(removeUser)
+      navigate("/login")
+    } catch (err) {}
+  };
 
   return (
     <div className="navbar bg-base-300">
       <div className="flex-1">
-        <Link to="/" className="btn btn-ghost text-xl">🧑‍💻DevTinder</Link>
+        <Link to="/" className="btn btn-ghost text-xl">
+          🧑‍💻DevTinder
+        </Link>
       </div>
       {user && (
         <div className="flex-none gap-2">
@@ -38,7 +52,7 @@ const NavBar = () => {
                 <a>Settings</a>
               </li>
               <li>
-                <a>Logout</a>
+                <Link onClick={handleLogout}>Logout</Link>
               </li>
             </ul>
           </div>
