6d45577c4af45743909520a3fca86afc7487f488
sanjivks <sanjivshinde4@gmail.com>
2025-02-12 13:01:55 +0530

    if not authenticated then redirect to login page

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,8 +1,38 @@
 import NavBar from "../components/NavBar";
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
 import Footer from "../components/Footer";
+import { useDispatch, useSelector } from "react-redux";
+import axios from "axios";
+import { BASE_URL } from "../utils/constants";
+import { useEffect } from "react";
+import { addUser } from "../utils/userSlice";
 
 const Body = () => {
+  const userData = useSelector((store) => store.user);
+  const navigate = useNavigate();
+  const dispatch=useDispatch()
+
+  const ferchUser = async () => {
+    if (userData) return;
+    try {
+      const res = await axios.get(BASE_URL + "/profile/view", {
+        withCredentials: true,
+      });
+      console.log(res.data)
+      dispatch(addUser(res.data))
+    } catch (err) {
+      if(err.status===401){
+        navigate("/login");
+      }
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    ferchUser();
+  }, []);
+
+
   return (
     <div>
       <NavBar />
